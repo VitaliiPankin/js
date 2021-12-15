8745c4ca13ca1e0852593c3a6e2d306d4f99b172
VitaliiPankin <niknap777@ukr.net>
2021-12-15 20:20:29 +0200

    1

---
+++ js/index.js
@@ -0,0 +1,18 @@
+'use strict';
+
+const num = 5 ;
+
+console.log(num);
+
+// todo Example 4 - Класс Math
+
+/*
+ * Напиши скрипт, который выводит в консоль округленные вверх / 
+вниз и т.д.значения переменной value.Используй методы Math.floor(),
+ Math.ceil() и Math.round().Проверь что будет в консоли при значениях 
+ 27.3 и 27.9.
+ */
+
+const value = 27.6;
+console.log(Math.round(value));
+
